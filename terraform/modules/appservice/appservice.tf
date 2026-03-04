75d1a7239850feac39747fe687cd92f7f492bc75
sriveerapaneni <veerapanenikiranmaye@gmail.com>
2026-03-04 16:00:53 -0600

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,3 +1,9 @@
+resource "random_string" "app_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
@@ -10,7 +16,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "${var.application_type}-${var.resource_type}"
+  name                = "${var.application_type}-${var.resource_type}-${random_string.app_suffix.result}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   app_service_plan_id = azurerm_app_service_plan.test.id

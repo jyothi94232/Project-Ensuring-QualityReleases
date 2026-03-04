a05508be23566c1132c591c59f47f18fccee9f70
sriveerapaneni <veerapanenikiranmaye@gmail.com>
2026-03-04 15:34:13 -0600

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -15,7 +15,8 @@ resource "azurerm_linux_virtual_machine" "test" {
   name                = "${var.application_type}-${var.resource_type}-vm"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  size                = "Standard_F1s"
+  size                = "Standard_B2s"
+  zone                = "3"
   admin_username      = "${var.admin_username}"
   network_interface_ids = [
     azurerm_network_interface.test.id
